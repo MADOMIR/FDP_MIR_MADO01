2c87b8c696fa6ce4ce7a2983c735ff071db47591
Manuel Diaz <manualediazoliva@gmail.com>
2022-03-03 18:49:23 -0600

    PruebasParaTareaSemana5

---
+++ Semana5/PruebaFor.py
@@ -0,0 +1,4 @@
+Numero=int(input("Ingrese un numero"))
+for i in range(1,Numero+1):
+    print(i)
+    
---
+++ Semana5/PruebaOpen.py
@@ -0,0 +1,5 @@
+
+InputFile=input("Ingrese la ruta donde requiere guardar los archivos:")
+i=1
+archivo=open(InputFile +"\Semana"+ str(i) + ".txt","w")
+archivo.close()
