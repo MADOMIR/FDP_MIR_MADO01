a2eb55def327d351baf64004add38b8b2bd64325
Manuel Diaz <manualediazoliva@gmail.com>
2022-03-05 21:46:30 -0600

    fin de tarea

--- Semana6/Opcion1.py
+++ Semana6/Opcion1.py
@@ -1,5 +1,28 @@
 import datetime
 def ImpDiaHora():
+    ListaDias={
+        0:"domingo",
+        1:"lunes",
+        2:"martes",
+        3:"miercoles",
+        4:"jueves",
+        5:"viernes",
+        6:"sábado",  
+    }
+    ListaMeses={
+        1:"Enero",
+        2:"Febrero",
+        3:"Marzo",
+        4:"Abril",
+        5:"Mayo",
+        6:"Junio",
+        7:"Julio",
+        8:"Agosto",
+        9:"Septiembre",
+        10:"Octubre",
+        11:"Noviembre",
+        12:"Diciembre",
+    }
     FechaHora = datetime.datetime.now()
-    print("\nHoy es ",FechaHora.strftime("%A"), " ", FechaHora.strftime("%d") ," de",FechaHora.strftime("%B"), " del año", FechaHora.strftime("%Y"))
+    print("\nHoy es ",ListaDias.get(int(FechaHora.strftime("%w"))), " ", FechaHora.strftime("%d") ," de",ListaMeses.get(int(FechaHora.strftime("%m"))), " del año", FechaHora.strftime("%Y"))
     print("y son las ",FechaHora.strftime("%I:%M%p"))
